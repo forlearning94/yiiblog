f09b0abbb9d2e17a58c382b517df9782c5cdd958
forlearning94 <ikrom21512@mail.ru>
2020-09-26 19:39:45 +0500

    sss

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -8,9 +8,10 @@ use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
-use app\assets\AppAsset;
+//use app\assets\AppAsset;
+use app\assets\PublicAsset;
 
-AppAsset::register($this);
+PublicAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
